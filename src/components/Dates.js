404b7f0d1a00d36b6c1a4d7171b2a92a931028b5
LBU <vvookly@gmail.com>
2021-09-12 19:44:04 -0400

    calendar icon added and datepick width adjusted

--- src/components/Dates.js
+++ src/components/Dates.js
@@ -8,9 +8,18 @@ const StyledDates = styled.div`
 	box-sizing: border-box;
 	display: flex;
 `;
+
+const StyledIcon = styled.div`
+	margin: 20px 5px 20px 20px;
+	font-size: 1.3rem;
+	@media (max-width: 720px) {
+		font-size: 16px;
+	}
+`;
 const StyledDatePicker = styled(DatePicker)`
-	margin: 20px;
+	margin: 20px 20px 20px 0px;
 	padding: 3px;
+	width: 120px;
 	font-size: 1.3rem;
 	cursor: pointer;
 	border: none;
@@ -25,13 +34,17 @@ const StyledDatePicker = styled(DatePicker)`
 		box-shadow: rgba(0, 0, 0, 2) 3px 3px 3px 0px;
 	}
 	@media (max-width: 720px) {
-		font-size: 13px;
+		width: 100px;
+		font-size: 16px;
 	}
 `;
 
 const Dates = ({ setSelectedDate, selectedDate }) => {
 	return (
 		<StyledDates>
+			<StyledIcon>
+				<i class="far fa-calendar-alt"></i>
+			</StyledIcon>
 			<StyledDatePicker
 				selected={selectedDate}
 				onChange={(date) => setSelectedDate(date)}
